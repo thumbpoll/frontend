0fba3866b476d95d78efac10e66048797758efa4
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-06 22:06:18 +0700

    initial commit

---
+++ pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Menu from "../components/HomeMenu";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Menu />
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import Home from "../pages/Home";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>{/* <Route path="/" exact component={Home} /> */}</Switch>
+      </Router>
+    );
+  }
+}
+
+export default App;
