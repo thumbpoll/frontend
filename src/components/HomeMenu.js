2e90f4f234d1bff89a585f21fa3dd492fcc38144
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-07 17:09:49 +0700

    edit home page, add jumbotron-like antd version, install emotion.

--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { Menu, Icon } from "antd";
-
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+import { Menu } from "antd";
 
 class MenuHome extends React.Component {
   state = {
@@ -22,41 +19,11 @@ class MenuHome extends React.Component {
         onClick={this.handleClick}
         selectedKeys={[this.state.current]}
         mode="horizontal"
+        style={{ backgroundColor: "#75C4FF", color: "#fff" }}
       >
-        <Menu.Item key="mail">
-          <Icon type="mail" />
-          Navigation One
-        </Menu.Item>
-        <Menu.Item key="app" disabled>
-          <Icon type="appstore" />
-          Navigation Two
-        </Menu.Item>
-        <SubMenu
-          title={
-            <span className="submenu-title-wrapper">
-              <Icon type="setting" />
-              Navigation Three - Submenu
-            </span>
-          }
-        >
-          <MenuItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
-          </MenuItemGroup>
-          <MenuItemGroup title="Item 2">
-            <Menu.Item key="setting:3">Option 3</Menu.Item>
-            <Menu.Item key="setting:4">Option 4</Menu.Item>
-          </MenuItemGroup>
-        </SubMenu>
-        <Menu.Item key="alipay">
-          <a
-            href="https://ant.design"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
+        <Menu.Item key="about">About us</Menu.Item>
+        <Menu.Item key="login">Login</Menu.Item>
+        <Menu.Item key="signup">Sign up</Menu.Item>
       </Menu>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,40 @@
+import Logo from "../assets/logos/thumbpoll-logo.png";
 import React, { Component } from "react";
+import { Layout } from "antd";
 import MenuHome from "../components/HomeMenu";
+import RideThumb from "../assets/images/ride-thumb.png";
+const { Content, Footer } = Layout;
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <MenuHome />
+      <div style={{ width: 480, margin: "0 auto", backgroundColor: "#75C4FF" }}>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <img src={Logo} alt="logo" style={{ width: 158, height: 59 }} />
+          <MenuHome />
+        </div>
+        <Content
+          style={{
+            backgroundColor: "#75C4FF",
+            minHeight: 30,
+            display: "flex",
+            justifyContent: " space-between",
+            alignItems: "center",
+            padding: "10px 10px"
+          }}
+        >
+          <h1 style={{ color: "white" }}>
+            Let's poll <br /> with our thumb
+          </h1>
+          <img
+            src={RideThumb}
+            alt="ride-thumb"
+            style={{ width: "219px", height: "187px", right: "0" }}
+          />
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
       </div>
     );
   }
