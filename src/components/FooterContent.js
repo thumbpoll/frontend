117d49200849c4d22c91b7db794504131dd40465
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-08 01:02:57 +0700

    edit footer content

---
+++ src/components/FooterContent.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+
+export default class FooterContent extends Component {
+  render() {
+    return (
+      <div>
+        <span style={{ margin: "5px" }}>English (UK)</span>
+        <span style={{ margin: "5px", color: "#000" }}>Bahasa Indonesia</span>
+        <span style={{ margin: "5px", color: "#000" }}>Bahasa Sunda</span>
+        <span style={{ margin: "5px", color: "#000" }}>Basa Jawa</span>
+        <span style={{ margin: "5px", color: "#000" }}>日本語</span>
+        <span style={{ margin: "5px", color: "#000" }}>Français (France)</span>
+        <span style={{ margin: "5px", color: "#000" }}>Español</span>
+        <span style={{ margin: "5px", color: "#000" }}>한국어</span>
+        <span style={{ margin: "5px", color: "#000" }}>Português (Brasil)</span>
+        <span style={{ margin: "5px", color: "#000" }}>Deutsch</span>
+        <br />
+        <br />
+        <span style={{ margin: "5px", color: "#000" }}>About</span>
+        <span style={{ margin: "5px", color: "#000" }}>Help Center</span>
+        <span style={{ margin: "5px", color: "#000" }}>Blog</span>
+        <span style={{ margin: "5px", color: "#000" }}>Status</span>
+        <span style={{ margin: "5px", color: "#000" }}>Jobs</span>
+        <span style={{ margin: "5px", color: "#000" }}>Terms</span>
+        <span style={{ margin: "5px", color: "#000" }}>Privacy Policy</span>
+        <span style={{ margin: "5px", color: "#000" }}>Cookies</span>
+        <span style={{ margin: "5px", color: "#000" }}>Apps</span>
+        <span style={{ margin: "5px", color: "#000" }}>Businesses</span>
+        <span style={{ margin: "5px", color: "#000" }}>Developers</span>
+        <span style={{ margin: "5px", color: "#000" }}>Setting</span>
+        <span style={{ margin: "5px", color: "#000" }}>Career</span>
+        <span style={{ margin: "5px", color: "#000" }}>Account Security</span>
+        <br />
+        <br />
+        <span style={{ color: "#000" }}>Thumbpoll © 2019</span>
+      </div>
+    );
+  }
+}
