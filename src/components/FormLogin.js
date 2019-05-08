06b529f3f2fbf5a3138d9775fe0e63e524cf7af8
darlyn <codejs@outlook.com>
2019-05-08 17:06:54 +0700

    improve form login

---
+++ src/components/FormLogin.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { Form, Icon, Input, Button, Checkbox } from "antd";
+
+class NormalLoginForm extends React.Component {
+  state = {
+    size: "large"
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  render() {
+    const size = this.state.size;
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "Please input your username!" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input your Password!" }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("remember", {
+            valuePropName: "checked",
+            initialValue: true
+          })(<Checkbox>Remember me</Checkbox>)}
+          <a className="login-form-forgot" href="login">
+            Forgot password
+          </a>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            shape="round"
+            value="large"
+            size={size}
+          >
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create({ name: "normal_login" })(
+  NormalLoginForm
+);
+
+export default WrappedNormalLoginForm;
