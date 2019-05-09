c6f23ab3911681115c15d81e42b21201daff2cda
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-09 12:35:32 +0700

    add after login navbar component

---
+++ src/components/NavbarAfterLogin.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Menu, Avatar } from "antd";
+
+class NavbarDashboard extends React.Component {
+  state = {
+    current: "polls"
+  };
+
+  handleClick = e => {
+    console.log("click ", e);
+    this.setState({
+      current: e.key
+    });
+  };
+
+  render() {
+    return (
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Menu
+          onClick={this.handleClick}
+          selectedKeys={[this.state.current]}
+          mode="horizontal"
+        >
+          <Menu.Item key="polls">Polls</Menu.Item>
+          <Menu.Item key="participants">Participants</Menu.Item>
+          <Menu.Item key="results">
+            <a
+              href="https://ant.design"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Results
+            </a>
+          </Menu.Item>
+        </Menu>
+        <Avatar
+          style={{ backgroundColor: "#87d068", margin: "10px 10px" }}
+          icon="user"
+        />
+      </div>
+    );
+  }
+}
+
+export default NavbarDashboard;
