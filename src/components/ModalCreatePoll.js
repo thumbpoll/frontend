8eebcd4ac6581fa0ec6d476eff3250307196162b
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-09 15:30:18 +0700

    add a modal for create poll

---
+++ src/components/ModalCreatePoll.js
@@ -0,0 +1,183 @@
+import React from "react";
+import { Button, Modal, Form, Input, Icon, Radio } from "antd";
+
+let id = 0;
+const CreatePollForm = Form.create({ name: "form_in_modal" })(
+  // eslint-disable-next-line
+  class extends React.Component {
+    remove = k => {
+      const { form } = this.props;
+      // can use data-binding to get
+      const keys = form.getFieldValue("keys");
+      // We need at least 1 Option
+      if (keys.length === 1) {
+        return;
+      }
+
+      // can use data-binding to set
+      form.setFieldsValue({
+        keys: keys.filter(key => key !== k)
+      });
+    };
+
+    add = () => {
+      const { form } = this.props;
+      // can use data-binding to get
+      const keys = form.getFieldValue("keys");
+      const nextKeys = keys.concat(id++);
+      // can use data-binding to set
+      // important! notify form to detect changes
+      form.setFieldsValue({
+        keys: nextKeys
+      });
+    };
+    render() {
+      const { getFieldDecorator, getFieldValue } = this.props.form;
+      const formItemLayout = {
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 4 }
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 20 }
+        }
+      };
+      const formItemLayoutWithOutLabel = {
+        wrapperCol: {
+          xs: { span: 24, offset: 0 },
+          sm: { span: 20, offset: 4 }
+        }
+      };
+      getFieldDecorator("keys", { initialValue: [1] });
+      const keys = getFieldValue("keys");
+      const formItems = keys.map((k, index) => (
+        <Form.Item
+          {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
+          label={index === 0 ? "Option" : ""}
+          required={true}
+          key={k}
+        >
+          {getFieldDecorator(`names[${k}]`, {
+            validateTrigger: ["onChange", "onBlur"],
+            rules: [
+              {
+                required: true,
+                whitespace: true,
+                message: "Please input option or delete this field."
+              }
+            ]
+          })(
+            <Input
+              placeholder="option"
+              style={{ width: "60%", marginRight: 8 }}
+            />
+          )}
+          {keys.length > 1 ? (
+            <Icon
+              className="dynamic-delete-button"
+              type="minus-circle-o"
+              onClick={() => this.remove(k)}
+            />
+          ) : null}
+        </Form.Item>
+      ));
+      const { visible, onCancel, onCreate, form } = this.props;
+
+      return (
+        <Modal
+          visible={visible}
+          title="Create a Poll"
+          okText="Create"
+          onCancel={onCancel}
+          onOk={onCreate}
+          style={{ width: 480 }}
+        >
+          <Form layout="vertical">
+            <Form.Item label="Title">
+              {getFieldDecorator("title", {
+                rules: [
+                  {
+                    required: true,
+                    message: "Please input the title of Poll!"
+                  }
+                ]
+              })(<Input />)}
+            </Form.Item>
+            <Form onSubmit={this.handleSubmit}>
+              {formItems}
+              <Form.Item {...formItemLayoutWithOutLabel}>
+                <Button
+                  type="dashed"
+                  onClick={this.add}
+                  style={{ width: "60%" }}
+                >
+                  <Icon type="plus" /> Add options
+                </Button>
+              </Form.Item>
+              <Form.Item {...formItemLayoutWithOutLabel} />
+            </Form>
+            <Form.Item className="collection-create-form_last-form-item">
+              {getFieldDecorator("modifier", {
+                initialValue: "public"
+              })(
+                <Radio.Group>
+                  <Radio value="private">Private</Radio>
+                </Radio.Group>
+              )}
+            </Form.Item>
+          </Form>
+        </Modal>
+      );
+    }
+  }
+);
+
+class PollModal extends React.Component {
+  state = {
+    visible: false
+  };
+
+  showModal = () => {
+    this.setState({ visible: true });
+  };
+
+  handleCancel = () => {
+    this.setState({ visible: false });
+  };
+
+  handleCreate = () => {
+    const form = this.formRef.props.form;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+
+      console.log("Received values of form: ", values);
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  };
+
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+
+  render() {
+    return (
+      <div>
+        <Button type="primary" onClick={this.showModal}>
+          Create a Poll
+        </Button>
+        <CreatePollForm
+          wrappedComponentRef={this.saveFormRef}
+          visible={this.state.visible}
+          onCancel={this.handleCancel}
+          onCreate={this.handleCreate}
+        />
+      </div>
+    );
+  }
+}
+
+export default PollModal;
