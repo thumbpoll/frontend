3d1319f3d6f09f26e42b9d125926eb34e7d3f77a
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-15 12:34:50 +0700

    set the action in this page up & connect 'em

--- src/pages/dashboard/AfterLogin.js
+++ src/pages/dashboard/AfterLogin.js
@@ -4,7 +4,9 @@ import LogoMini from "../../assets/logos/thumbpoll-logo-mini.png";
 import NavbarDashboard from "../../components/dashboard/NavbarAfterLogin";
 import PollModal from "../../components/dashboard/ModalCreatePoll";
 import { Layout, Menu, Empty, Icon } from "antd";
-import { Link } from "react-router-dom";
+import { Link, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { login } from "../../redux/actions/profileAction";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -18,7 +20,9 @@ class Dashboard extends React.Component {
   };
 
   render() {
-    return (
+    return !this.props.isAuthenticated ? (
+      <Redirect to="/login" />
+    ) : (
       <div
         style={{
           width: 480,
@@ -93,4 +97,11 @@ class Dashboard extends React.Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = store => ({
+  isAuthenticated: store.profile.isAuthenticated
+});
+
+export default connect(
+  mapStateToProps,
+  { login }
+)(withRouter(Dashboard));
