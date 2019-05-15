082634f38cfafb67eed17fa851fe73f5545b9092
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-15 12:36:28 +0700

    add login action case

--- src/redux/reducers/profileReducer.js
+++ src/redux/reducers/profileReducer.js
@@ -8,6 +8,12 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case "login":
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true
+      };
     default:
       return state;
   }
