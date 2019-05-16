07f3976bfee6b40254fb2f4bd3687e84b79b7d01
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-16 20:40:15 +0700

    add create to root reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import profileReducer from "./profileReducer";
 import registerReducer from "./register";
 import polls from "./polls";
+import createPoll from "./create";
 
 const rootReducers = combineReducers({
   profile: profileReducer,
   register: registerReducer,
-  polls
+  polls,
+  createPoll
 });
 
 export default rootReducers;
