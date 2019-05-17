690e7c78a4e4392e4d299ca25d1dbf739ceb9b12
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-05-17 15:51:53 +0700

    fix can't open from generaly devices

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,16 +2,21 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducers from "./reducers";
 
-const initialState = {};
+// const initialState = {};
+
 const middleware = [thunk];
+const composeEnhancers =
+  process.env.NODE_ENV === "development" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
 const store = createStore(
   rootReducers,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  // initialState,
+  composeEnhancers(applyMiddleware(...middleware))
 );
 
 export default store;
